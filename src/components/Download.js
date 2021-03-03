35dc5dfdb63dd295be3cd0e9ec3694651632f1f5
ippee <ippee.music@gmail.com>
2021-03-04 03:57:58 +0900

    add background-image for each components

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,14 +1,28 @@
-function Download() {
-  return (
-    <div className="download">
-      <h1>ダウンロード</h1>
-      <div className="windows"><button onClick="">Windows</button></div>
-      <div className="mac"><button onClick="">macOS</button></div>
-      <div className="spec">
-        <p>このゲームはWindows 10 Home、およびmacOS Catalina 10.15.7にて開発されました。</p>
+import React from 'react';
+
+import fireworks from '../assets/img/fireworks.jpg';
+
+class Download extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      downloadStyle: { backgroundImage: `url(${fireworks})` }
+    };
+  }
+
+  render() {
+    return (
+      <div className="download" style={this.state.downloadStyle}>
+        <h1>ダウンロード</h1>
+        <div className="windows"><button onClick="">Windows</button></div>
+        <div className="mac"><button onClick="">macOS</button></div>
+        <div className="spec">
+          <p>このゲームはWindows 10 Home、およびmacOS Catalina 10.15.7にて開発されました。</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Download;
