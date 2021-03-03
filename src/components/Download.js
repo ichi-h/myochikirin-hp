c1b9f52ce02efdd4b87149cbc2fbb2e67301ff87
ippee <ippee.music@gmail.com>
2021-03-04 01:47:31 +0900

    update download

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,6 +1,13 @@
 function Download() {
   return (
-    <div className="download">ダウンロードやで</div>
+    <div className="download">
+      <h1>ダウンロード</h1>
+      <div className="windows"><button onClick="">Windows</button></div>
+      <div className="mac"><button onClick="">macOS</button></div>
+      <div className="spec">
+        <p>このゲームはWindows 10 Home、およびmacOS Catalina 10.15.7にて開発されました。</p>
+      </div>
+    </div>
   );
 }
 
