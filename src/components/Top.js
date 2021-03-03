5cfd1a3817528a9ca9a3d1ee32e3cbc8c1626376
ippee <ippee.music@gmail.com>
2021-03-04 03:45:39 +0900

    add background-image

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,7 +1,22 @@
-function Top() {
-  return (
-    <div className="top">トップやで</div>
-  );
+import React from 'react';
+
+import theme from '../assets/img/theme.jpg';
+import '../assets/css/Top.css';
+
+class Top extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      topStyle: { backgroundImage: `url(${theme})` }
+    };
+  }
+
+  render() {
+    return (
+      <div className="top" style={this.state.topStyle} />
+    );
+  }
 }
 
 export default Top;
