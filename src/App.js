585345d04012931e4ffe066e45d4c3eb986c8daa
ippee <ippee.music@gmail.com>
2021-03-04 03:17:54 +0900

    fix navbar style

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import './App.css';
@@ -7,18 +8,41 @@ import Overview from './components/Overview.js'
 import Download from './components/Download.js'
 import Credit from './components/Credit.js'
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Route path="/" exact component={Top}></Route>
-        <Route path="/overview" component={Overview}></Route>
-        <Route path="/download" component={Download}></Route>
-        <Route path="/credit" component={Credit}></Route>
-        <Navbar />
-      </BrowserRouter>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      appStyle: {}
+    }
+  }
+
+  componentDidMount() {
+    let docElm = document.documentElement;
+    let clientWidth = docElm.clientWidth;
+    let clientHeihgt = docElm.clientHeight;
+
+    let style = {
+      width: clientWidth,
+      height: clientHeihgt
+    }
+
+    this.setState({ appStyle: style })
+  }
+
+  render() {
+    return (
+      <div className="App" style={this.state.appStyle}>
+        <BrowserRouter>
+          <Route path="/" exact component={Top}></Route>
+          <Route path="/overview" component={Overview}></Route>
+          <Route path="/download" component={Download}></Route>
+          <Route path="/credit" component={Credit}></Route>
+          <Navbar />
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
