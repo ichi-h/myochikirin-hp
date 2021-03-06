9915b0c5d10e5354a6a093e5230587cfbd9d07c2
ippee <ippee.music@gmail.com>
2021-03-06 12:10:03 +0900

    add download function

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import firebase from 'firebase'
 
 import '../assets/css/Download.css'
 import bar from '../assets/img/bar.jpg';
@@ -12,6 +13,27 @@ class Download extends React.Component {
     };
   }
 
+  downloadFile(filePath) {
+    let storage = firebase.storage();
+    let storageRef = storage.ref();
+    let spaceRef = storageRef.child(filePath);
+
+    spaceRef.getDownloadURL().then(function(url) {
+      let xhr = new XMLHttpRequest();
+      xhr.responseType = 'blob';
+      xhr.onload = function(event) {
+        // let blob = xhr.response;
+      };
+      xhr.open('GET', url);
+      xhr.send();
+
+      window.location.href = url;
+
+    }).catch(function(error) {
+      alert(error.message);
+    })
+  }
+
   render() {
     return (
       <div className="download">
@@ -19,12 +41,12 @@ class Download extends React.Component {
         <div className="content">
           <h1>ダウンロード</h1>
           <div className="windows">
-            <a className="download-button" href="https://drive.google.com/file/d/1EpP1-4gOh90B9J5_vCKV4oNER7B5NZWE/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+            <a className="download-button" href="javascript:void(0);" onClick={() => this.downloadFile("めうちきりんのメトロポリス_for_win.zip")}>
               <i className="icon-windows" /> Windows版 ダウンロード
             </a>
           </div>
           <div className="mac">
-            <a className="download-button" href="https://drive.google.com/file/d/1ngekNsKTUR6KXRowd3qQcs3ozQ42TZ1W/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+            <a className="download-button" href="javascript:void(0);" onClick={() => this.downloadFile("めうちきりんのメトロポリス_for_mac.zip")}>
               <i className="icon-apple" /> macOS版 ダウンロード
             </a>
           </div>
