4c8fd84e5c11603a2fe4884fd42c677c2656c39d
ippee <ippee.music@gmail.com>
2021-04-05 02:01:53 +0900

    レイアウト調整

--- src/components/Spec.js
+++ src/components/Spec.js
@@ -18,16 +18,18 @@ class Spec extends React.Component {
         <div className="download bg" style={this.state.specStyle} />
         <div className="content">
           <h1>スペック</h1>
-          <h2>タイトル</h2>
-          <p>めうちきりんのメトロポリス</p>
-          <h2>ジャンル</h2>
-          <p>ノベルゲーム</p>
-          <h2>作者</h2>
-          <p>いっぺー</p>
-          <h2>価格</h2>
-          <p>無料</p>
-          <h2>対応OS</h2>
-          <p>Windows / macOS</p>
+          <div className="spec-summary">
+            <h2>タイトル</h2>
+            <p>めうちきりんのメトロポリス</p>
+            <h2>ジャンル</h2>
+            <p>ノベルゲーム</p>
+            <h2>作者</h2>
+            <p>いっぺー</p>
+            <h2>価格</h2>
+            <p>無料</p>
+            <h2>対応OS</h2>
+            <p>Windows / macOS</p>
+          </div>
         </div>
       </div>
     );
