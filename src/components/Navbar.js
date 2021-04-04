4d796035bbc6d6e79961286fde773e41904a5677
ippee <ippee.music@gmail.com>
2021-04-04 23:57:53 +0900

    classNameに修正

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,19 +5,19 @@ import '../assets/css/Navbar.css';
 function Navbar() {
   return(
     <div className="navbar">
-      <div class="navbar-item">
+      <div className="navbar-item">
         <Link to="/">トップ</Link>
       </div>
-      <div class="navbar-item">
+      <div className="navbar-item">
         <Link to="/news">お知らせ</Link>
       </div>
-      <div class="navbar-item">
+      <div className="navbar-item">
         <Link to="/worldview">世界観</Link>
       </div>
-      <div class="navbar-item">
+      <div className="navbar-item">
         <Link to="/download">ダウンロード</Link>
       </div>
-      <div class="navbar-item">
+      <div className="navbar-item">
         <Link to="/spec">スペック</Link>
       </div>
     </div>
