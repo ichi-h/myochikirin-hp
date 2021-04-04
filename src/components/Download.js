e07178dc2cec83eb00992a5b3773a0f516f0d50c
ippee <ippee.music@gmail.com>
2021-04-05 00:50:04 +0900

    使用するFirebaseSDKのみをimport

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/storage';
 
 import '../assets/css/Download.css'
 import bar from '../assets/img/bar.jpg';
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import firebase from 'firebase/app';
-import 'firebase/firebase-storage';
 import './index.css';
 import './fontello.css'
 import App from './App';
