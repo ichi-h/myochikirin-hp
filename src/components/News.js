9be12baba6b827dac45a852e3e99b92324955363
ippee <ippee.music@gmail.com>
2021-10-10 17:05:12 +0900

    お知らせの更新

--- src/components/News.js
+++ src/components/News.js
@@ -21,6 +21,14 @@ class News extends React.Component {
           <div className="news-list">
             <ul>
               <div className="list-item">
+                <li>2021/10/10: v1.0.3の公開</li>
+                <ul>
+                  <li>画像閲覧時に、メニューバーが表示されるバグの修正</li>
+                  <li>スライドバーのデザインが崩れるバグを修正</li>
+                  <li>Windows 11、macOS Big Surへの対応</li>
+                </ul>
+              </div>
+              <div className="list-item">
                 <li>2021/5/27: v1.0.2の公開</li>
                 <ul>
                   <li>コードの修正</li>
